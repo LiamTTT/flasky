9b569396e230b1e1daaa6ddc9ac967fba4882848
LiamTTT <liam.liusibo@gmail.com>
2021-04-05 16:09:07 +0800

    use request.

--- scripts/hello.py
+++ scripts/hello.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    user_agent = request.headers.get('User-Agent')
     return '<h1>Hello World!<h1/>'
 
 @app.route('/user/<name>')
