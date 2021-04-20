386130982503b11f216bf8cdffd90ef6234d68e9
LiamTTT <liam.liusibo@gmail.com>
2021-04-20 20:44:40 +0800

    finish ch8, finish user management module.

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -12,7 +12,6 @@ class LoginForm(FlaskForm):
     submit = SubmitField('Log In')
 
 
-
 class RegistrationForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Length(1, 64), Email()])
     username = StringField('Username', validators=[
@@ -36,3 +35,36 @@ class RegistrationForm(FlaskForm):
     def validate_username(self, filed):
         if User.query.filter_by(username=filed.data).first():
             raise ValidationError('Username already in use.')
+
+
+class ChangePasswordForm(FlaskForm):
+    old_password = PasswordField('Old password', validators=[DataRequired()])
+    password = PasswordField('New password', validators=[
+        DataRequired(), EqualTo('password2', message='Passwords must match.')])
+    password2 = PasswordField('Confirm new password',
+                              validators=[DataRequired()])
+    submit = SubmitField('Update Password')
+
+
+class PasswordResetRequestForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Length(1, 64),
+                                             Email()])
+    submit = SubmitField('Reset Password')
+
+
+class PasswordResetForm(FlaskForm):
+    password = PasswordField('New Password', validators=[
+        DataRequired(), EqualTo('password2', message='Passwords must match')])
+    password2 = PasswordField('Confirm password', validators=[DataRequired()])
+    submit = SubmitField('Reset Password')
+
+
+class ChangeEmailForm(FlaskForm):
+    email = StringField('New Email', validators=[DataRequired(), Length(1, 64),
+                                                 Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Update Email Address')
+
+    def validate_email(self, field):  # 要检查新的email是否存在
+        if User.query.filter_by(email=field.data.lower()).first():
+            raise ValidationError('Email already registered.')
