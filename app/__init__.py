538f4a55dd912521cd53a791e55b57c3d698a88c
LiamTTT <liam.liusibo@gmail.com>
2021-04-20 19:30:31 +0800

    add register.

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,16 @@ from flask_bootstrap import Bootstrap
 from flask_mail import Mail
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from config import config
 
 bootstrap = Bootstrap()
 mail = Mail()
 moment = Moment()
 db = SQLAlchemy()
+
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
 
 ''' 将应用的创建挪到工厂函数之中
 '''
@@ -21,9 +25,14 @@ def create_app(config_name):
     mail.init_app(app)
     moment.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
 
     # 路由和错误页面
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)  # 当蓝本注册到app上时，定义于蓝本中的路由和错误处理程序才生效
 
+    # auth
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
+    
     return app
